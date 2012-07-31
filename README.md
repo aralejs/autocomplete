029d4274e4282179f3b2c18ae82338a1fa57ea41
popomore <sakura9515@gmail.com>
2012-07-31 23:22:11 +0800

    init module

---
+++ README.md
@@ -0,0 +1,5 @@
+# Autocomplete
+
+// description
+
+---
---
+++ tests/autocomplete-spec.js
@@ -0,0 +1,13 @@
+define(function(require) {
+
+    var Autocomplete = require('../src/autocomplete');
+
+    describe('Autocomplete', function() {
+
+        test('normal usage', function() {
+
+        });
+    });
+
+});
+
