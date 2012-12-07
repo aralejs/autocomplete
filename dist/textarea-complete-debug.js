5467919a557941bba59d8857e4e3f7e41f130860
popomore <sakura9515@gmail.com>
2012-12-07 16:42:52 +0800

    rebuild

--- dist/textarea-complete-debug.js
+++ dist/textarea-complete-debug.js
@@ -1,157 +1,3 @@
-define("arale/autocomplete/1.0.0/data-source-debug", ["arale/base/1.0.1/base-debug", "arale/class/1.0.0/class-debug", "arale/events/1.0.0/events-debug", "$-debug"], function(require, exports, module) {
-
-    var Base = require('arale/base/1.0.1/base-debug');
-    var $ = require('$-debug');
-
-    var DataSource = Base.extend({
-
-        attrs: {
-            source: null,
-            type: 'array'
-        },
-
-        initialize: function(config) {
-            DataSource.superclass.initialize.call(this, config);
-
-            var source = this.get('source');
-            if (isString(source)) {
-                this.set('type', 'url');
-            } else if ($.isArray(source)) {
-                this.set('type', 'array');
-            } else if ($.isPlainObject(source)) {
-                this.set('type', 'object');
-            } else if ($.isFunction(source)) {
-                this.set('type', 'function');
-            } else {
-                throw new Error('Source Type Error');
-            }
-        },
-
-        getData: function(query) {
-            return this['_get' + capitalize(this.get('type')) + 'Data'](query);
-        },
-
-        _getUrlData: function(query) {
-            var that = this, options;
-            var url = this.get('source')
-                .replace(/{{query}}/g, query ? encodeURIComponent(query) : '');
-            if (/^(https?:\/\/)/.test(url)) {
-                options = {dataType: 'jsonp'};
-            } else {
-                options = {dataType: 'json'};
-            }
-            $.ajax(url, options)
-                .success(function(data) {
-                    that.trigger('data', data);
-                })
-                .error(function(data) {
-                    that.trigger('data', {});
-                });
-        },
-
-        _getArrayData: function() {
-            var source = this.get('source');
-            this.trigger('data', source);
-            return source;
-        },
-
-        _getObjectData: function(query) {
-            var source = this.get('source');
-            this.trigger('data', source);
-            return source;
-        },
-
-        _getFunctionData: function(query) {
-            var func = this.get('source');
-            // 如果返回 false 可阻止执行
-            var data = func.call(this, query);
-            if (data) {
-                this.trigger('data', data);
-            }
-        }
-    });
-
-
-    module.exports = DataSource;
-
-    function isString(str) {
-        return Object.prototype.toString.call(str) === '[object String]';
-    }
-
-    function capitalize(str) {
-        if (!str) {
-            return '';
-        }
-        return str.replace(
-            /^([a-z])/,
-            function(f, m) {
-                return m.toUpperCase();
-            }
-        );
-    }
-});
-
-
-define("arale/autocomplete/1.0.0/filter-debug", ["$-debug"], function(require, exports, module) {
-
-    var $ = require('$-debug');
-
-    var Filter = {
-        default: function(data, query, options) {
-            var result = [];
-            $.each(data, function(index, item) {
-                var o = {}, matchKey = getMatchKey(item, options);
-                if ($.isPlainObject(item)) {
-                    o = $.extend({}, item);
-                }
-                o.matchKey = matchKey;
-                result.push(o);
-            });
-            return result;
-        },
-
-        startsWith: function(data, query, options) {
-            var result = [], l = query.length,
-                reg = new RegExp('^' + query);
-            $.each(data, function(index, item) {
-                var o = {}, matchKey = getMatchKey(item, options);
-
-                if ($.isPlainObject(item)) {
-                    o = $.extend({}, item);
-                }
-                // 生成 item
-                // {
-                //   ...   // self property
-                //   matchKey: '', // 匹配的内容
-                //   highlightIndex: [] // 高亮的索引
-                // }
-                if (reg.test(matchKey)) {
-                    o.matchKey = matchKey;
-                    if (l > 0) {
-                        o.highlightIndex = [[0, l]];
-                    }
-                    result.push(o);
-                }
-            });
-            return result;
-        }
-    };
-
-    module.exports = Filter;
-
-    function getMatchKey(item, options) {
-        if ($.isPlainObject(item)) {
-            // 默认取对象的 value 属性
-            var key = (options && options.key) || 'value'; 
-            return item[key] || '';
-        } else {
-            return item;
-        }
-    }
-});
-
-
-
 define("arale/autocomplete/1.0.0/autocomplete-debug", ["./data-source-debug", "./filter-debug", "$-debug", "arale/overlay/0.9.13/overlay-debug", "arale/position/1.0.0/position-debug", "arale/iframe-shim/1.0.0/iframe-shim-debug", "arale/widget/1.0.2/widget-debug", "arale/base/1.0.1/base-debug", "arale/class/1.0.0/class-debug", "arale/events/1.0.0/events-debug", "arale/widget/1.0.2/templatable-debug", "gallery/handlebars/1.0.0/handlebars-debug"], function(require, exports, module) {
 
     var $ = require('$-debug');
@@ -619,3 +465,317 @@ define("arale/autocomplete/1.0.0/autocomplete-debug", ["./data-source-debug", ".
         return data;
     }
 });
+
+define("arale/autocomplete/1.0.0/data-source-debug", ["arale/base/1.0.1/base-debug", "arale/class/1.0.0/class-debug", "arale/events/1.0.0/events-debug", "$-debug"], function(require, exports, module) {
+
+    var Base = require('arale/base/1.0.1/base-debug');
+    var $ = require('$-debug');
+
+    var DataSource = Base.extend({
+
+        attrs: {
+            source: null,
+            type: 'array'
+        },
+
+        initialize: function(config) {
+            DataSource.superclass.initialize.call(this, config);
+
+            var source = this.get('source');
+            if (isString(source)) {
+                this.set('type', 'url');
+            } else if ($.isArray(source)) {
+                this.set('type', 'array');
+            } else if ($.isPlainObject(source)) {
+                this.set('type', 'object');
+            } else if ($.isFunction(source)) {
+                this.set('type', 'function');
+            } else {
+                throw new Error('Source Type Error');
+            }
+        },
+
+        getData: function(query) {
+            return this['_get' + capitalize(this.get('type')) + 'Data'](query);
+        },
+
+        _getUrlData: function(query) {
+            var that = this, options;
+            var url = this.get('source')
+                .replace(/{{query}}/g, query ? encodeURIComponent(query) : '');
+            if (/^(https?:\/\/)/.test(url)) {
+                options = {dataType: 'jsonp'};
+            } else {
+                options = {dataType: 'json'};
+            }
+            $.ajax(url, options)
+                .success(function(data) {
+                    that.trigger('data', data);
+                })
+                .error(function(data) {
+                    that.trigger('data', {});
+                });
+        },
+
+        _getArrayData: function() {
+            var source = this.get('source');
+            this.trigger('data', source);
+            return source;
+        },
+
+        _getObjectData: function(query) {
+            var source = this.get('source');
+            this.trigger('data', source);
+            return source;
+        },
+
+        _getFunctionData: function(query) {
+            var func = this.get('source');
+            // 如果返回 false 可阻止执行
+            var data = func.call(this, query);
+            if (data) {
+                this.trigger('data', data);
+            }
+        }
+    });
+
+
+    module.exports = DataSource;
+
+    function isString(str) {
+        return Object.prototype.toString.call(str) === '[object String]';
+    }
+
+    function capitalize(str) {
+        if (!str) {
+            return '';
+        }
+        return str.replace(
+            /^([a-z])/,
+            function(f, m) {
+                return m.toUpperCase();
+            }
+        );
+    }
+});
+
+
+define("arale/autocomplete/1.0.0/filter-debug", ["$-debug"], function(require, exports, module) {
+
+    var $ = require('$-debug');
+
+    var Filter = {
+        default: function(data, query, options) {
+            var result = [];
+            $.each(data, function(index, item) {
+                var o = {}, matchKey = getMatchKey(item, options);
+                if ($.isPlainObject(item)) {
+                    o = $.extend({}, item);
+                }
+                o.matchKey = matchKey;
+                result.push(o);
+            });
+            return result;
+        },
+
+        startsWith: function(data, query, options) {
+            var result = [], l = query.length,
+                reg = new RegExp('^' + query);
+            $.each(data, function(index, item) {
+                var o = {}, matchKey = getMatchKey(item, options);
+
+                if ($.isPlainObject(item)) {
+                    o = $.extend({}, item);
+                }
+                // 生成 item
+                // {
+                //   ...   // self property
+                //   matchKey: '', // 匹配的内容
+                //   highlightIndex: [] // 高亮的索引
+                // }
+                if (reg.test(matchKey)) {
+                    o.matchKey = matchKey;
+                    if (l > 0) {
+                        o.highlightIndex = [[0, l]];
+                    }
+                    result.push(o);
+                }
+            });
+            return result;
+        }
+    };
+
+    module.exports = Filter;
+
+    function getMatchKey(item, options) {
+        if ($.isPlainObject(item)) {
+            // 默认取对象的 value 属性
+            var key = (options && options.key) || 'value'; 
+            return item[key] || '';
+        } else {
+            return item;
+        }
+    }
+});
+
+
+
+define("arale/autocomplete/1.0.0/textarea-complete-debug", ["./autocomplete-debug", "$-debug", "gallery/selection/0.9.0/selection-debug", "arale/overlay/0.9.13/overlay-debug", "arale/position/1.0.0/position-debug", "arale/iframe-shim/1.0.0/iframe-shim-debug", "arale/widget/1.0.2/widget-debug", "arale/base/1.0.1/base-debug", "arale/class/1.0.0/class-debug", "arale/events/1.0.0/events-debug", "arale/widget/1.0.2/templatable-debug", "gallery/handlebars/1.0.0/handlebars-debug"], function(require, exports, module) {
+
+  var $ = require('$-debug');
+  var selection = require('gallery/selection/0.9.0/selection-debug');
+  var AutoComplete = require('./autocomplete-debug');
+
+  var TextareaComplete = AutoComplete.extend({
+
+    attrs: {
+      cursor: false
+    },
+
+    setup: function() {
+      TextareaComplete.superclass.setup.call(this);
+      this.sel = selection(this.get('trigger'));
+
+      var inputFilter = this.get('inputFilter'), that = this;
+      this.set('inputFilter', function(val) {
+        var v = val.substring(0, that.sel.cursor()[1]);
+        return inputFilter.call(that, v);
+      });
+
+      if (this.get('cursor')) {
+        this.mirror = Mirror.init(this.get('trigger'));
+        this.dataSource.before('getData', function() {
+          that.mirror.setContent(
+            that.get('inputValue'),
+            that.queryValue,
+            that.sel.cursor()
+          );
+        });
+      }
+    },
+
+    _keyUp: function(e) {
+      if (this.get('visible')) {
+        e.preventDefault();
+        if (this.get('data').length) {
+          this._step(-1);
+        }
+      }
+    },
+
+    _keyDown: function(e) {
+      if (this.get('visible')) {
+        e.preventDefault();
+        if (this.get('data').length) {
+          this._step(1);
+        }
+      }
+    },
+
+    _keyEnter: function(e) {
+      // 如果没有选中任一一项也不会阻止
+      if (this.get('visible') && this.currentItem) {
+        e.preventDefault();
+        e.stopImmediatePropagation(); // 阻止冒泡及绑定的其他 keydown 事件
+        this.selectItem();
+      }
+    },
+
+    show: function() {
+      var cursor = this.get('cursor');
+      if (cursor) {
+        if ($.isArray(cursor)) {
+          var offset = cursor;
+        } else {
+          var offset = [0, 0];
+        }
+        var pos = this.mirror.getFlagRect();
+        var align = this.get('align');
+        align.baseXY = [pos.left + offset[0], pos.bottom + offset[1]];
+        align.selfXY = [0, 0];
+        this.set('align', align);
+      }
+      TextareaComplete.superclass.show.call(this);
+    },
+
+    selectItem: function() {
+      this.hide();
+
+      var item = this.currentItem,
+      index = this.get('selectedIndex'),
+      data = this.get('data')[index];
+
+      if (item) {
+        var matchKey = item.attr('data-value');
+        var right = this.sel.cursor()[1];
+        var left = right - this.queryValue.length;
+        this.sel
+          .cursor(left, right)
+          .text('')
+          .append(matchKey, 'right');
+
+        var value = this.get('trigger').val();
+        this.set('inputValue', value);
+        this.mirror && this.mirror.setContent(value, '', this.sel.cursor());
+        this.trigger('itemSelect', data);
+        this._clear();
+      }
+    }
+  });
+
+  // 计算光标位置
+  // MIT https://github.com/ichord/At.js/blob/master/js/jquery.atwho.js
+  var Mirror = {
+    mirror: null,
+    css: ["overflowY", "height", "width", "paddingTop", "paddingLeft", "paddingRight", "paddingBottom", "marginTop", "marginLeft", "marginRight", "marginBottom", 'fontFamily', 'borderStyle', 'borderWidth', 'wordWrap', 'fontSize', 'lineHeight', 'overflowX'],
+    init: function(origin) {
+      origin = $(origin);
+      var css = {
+        position: 'absolute',
+        left: -9999,
+        top: 0,
+        zIndex: -20000,
+        'white-space': 'pre-wrap'
+      };
+      $.each(this.css, function(i, p) {
+        return css[p] = origin.css(p);
+      });
+      this.mirror = $('<div><span></span></div>')
+        .css(css)
+        .insertAfter(origin);
+      return this;
+    },
+    setContent: function(content, query, cursor) {
+      var left = query ? (cursor[1] - query.length) : cursor[1];
+      var right = cursor[1];
+      var v = [
+        '<span>',
+          content.substring(0, left),
+        '</span>',
+        '<span id="flag">',
+          (query || ''),
+        '</span>',
+        '<span>',
+          content.substring(right),
+        '</span>'
+      ].join('');
+      this.mirror.html(v);
+      return this;
+    },
+    getFlagRect: function() {
+      var pos, rect, flag;
+      flag = this.mirror.find('span#flag');
+      pos = flag.position();
+      rect = {
+        left: pos.left,
+        right: flag.width() + pos.left,
+        top: pos.top,
+        bottom: flag.height() + pos.top
+      };
+      return rect;
+    }
+  };
+
+  module.exports = TextareaComplete;
+});
+
