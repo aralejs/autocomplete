42850350a97fc6fd0c175ece0187d1ed1f536950
popomore <sakura9515@gmail.com>
2013-01-17 15:17:45 +0800

    update testcase for #55

--- tests/issue-spec.js
+++ tests/issue-spec.js
@@ -1,6 +1,8 @@
 define(function(require) {
 
+  var $ = require('$');
   var Filter = require('../src/filter');
+  var AutoComplete = require('../src/autocomplete');
 
   describe('Issue', function() {
     it('#56 start with (', function() {
@@ -18,8 +20,23 @@ define(function(require) {
       ]);
     });
 
-    it('dont\'t change when hover', function() {
-    
+    it('#55 dont\'t change selectedIndex when hover', function() {
+      var ac, input = $('<input id="test" type="text" value="" />')
+        .appendTo(document.body);
+
+      ac = new AutoComplete({
+          trigger: '#test',
+          dataSource: ['abc', 'abd', 'cbd']
+      }).render();
+
+      ac.setInputValue('a');
+      var item = ac.$('li').eq(1);
+      item.mouseenter();
+      expect(item.hasClass('ui-autocomplete-item-hover')).to.be.ok();
+      expect(ac.get('selectedIndex')).to.be(-1);
+
+      input.remove();
+      ac.destroy();
     });
   });
 });
